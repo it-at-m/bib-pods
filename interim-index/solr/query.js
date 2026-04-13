e3ad0b3c10b8ffd3923345622752a28c7709c5e1
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-04-13 14:09:31 +0200

    Build interim index: from oai into solr

---
+++ interim-index/solr/query.js
@@ -0,0 +1,29 @@
+const SOLR_URL = "http://localhost:8983/solr/interim-index"
+
+const params1 = {
+    q:      "authors:Sapkowski",
+    fl:     "id,title,authors,year,subjects"
+}
+
+const params2 = {
+    q:      "genre:Musikdruck AND authors:Weill AND title:Kurt",
+    rows:   10,
+    start:  0,
+    fl:     "id,title,authors,year,genre"
+}
+
+async function query(params) {
+    const url = `${SOLR_URL}/select?${new URLSearchParams(params)}`
+    const res = await fetch(url)
+    const { response } = await res.json()
+    console.log(`Found ${response.numFound} results\n`)
+    for (const doc of response.docs) {
+        console.log(doc)
+    }
+}
+
+console.log("--- Query 1: by author ---")
+await query(params1)
+
+console.log("\n--- Query 2: genre + author + keyword ---")
+await query(params2)
