ff55b50f522f2ac3420dfa457e37c68371aa96d6
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-05-09 12:14:41 +0200

    Remove typo3 approach v14 composer site-package

--- typo3/bib-pods-ext/ext_emconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'bib-pods-ext',
-    'description' => 'bib-pods typo3 site package',
-    'category' => 'templates',
-    'version' => '0.1.0',
-    'state' => 'alpha',
-];
--- typo3/bib-pods-ext/ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Solid OIDC redirects back with these query parameters after login.
-// TYPO3 must ignore them during cHash validation, otherwise the redirect
-// landing page returns a 404 before any JS can process the tokens.
-$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'code';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'state';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'iss';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'session_state';
